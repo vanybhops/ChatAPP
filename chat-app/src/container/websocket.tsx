ab31f3d8ba681ddc02b535e7a13ced0cc5258e3d
vany <79766479+vanybhops@users.noreply.github.com>
2023-04-27 16:53:06 +0200

    forgor

--- chat-app/src/container/websocket.tsx
+++ chat-app/src/container/websocket.tsx
@@ -1,2 +1,2 @@
-const websocket = new WebSocket(`${window.location.protocol==="http:"?"ws://"+window.location.host:"ws://"+window.location.host}`)
+const websocket = new WebSocket(`ws://${window.location.host}`)
 export default websocket
